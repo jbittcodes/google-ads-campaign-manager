78629b159d0d5c492fd799bdd7338c3cb43d08c3
jbittcodes <jamesbitting76@gmail.com>
2024-09-18 09:50:34 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <h1>Google Ads Integration</h1>
+        <p>This project demonstrates Google Ads integration.</p>
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
